81c5b6165e17538df9f220293d4df0f89d226504
Mrudul Lole <mrudullole08@gmail.com>
2026-01-30 18:30:32 +0530

    1st

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -7,10 +7,7 @@ terraform {
     encrypt        = true
   }
   required_version = ">=0.13.0"
-  required_providers {
-    aws = {
-      version = ">= 2.7.0"
-      source  = "hashicorp/aws"
+  required_providers  = "hashicorp/aws"
     }
   }
 }
